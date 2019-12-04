d31a9b22f2d58282d8d9cc773480595613ffac55
Tyler Griggs <tgriggs@college.harvard.edu>
2019-12-03 22:18:29 -0500

    finished openblas setup

--- blas.c
+++ blas.c
@@ -6,25 +6,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <iostream>
-#include <Eigen/Dense>
-using Eigen::MatrixXd;
+#include <cblas.h>
 
 typedef struct matrix {
 	int height;
 	int width;
-	MatrixXd data;
+	float* data;
 } matrix;
 
 // reads matrix input from filename and stores matrices in *A and *B
-int build_and_multiply(matrix* A, matrix* B, char* filename) {
+int build_matrices(matrix* A, matrix* B, matrix* C, char* filename) {
 	FILE* mfile = fopen(filename, "r");
 	if (!mfile) {
 		printf("Could not open file: %s\n", filename);
 		return -1;
 	}
 
-	char* linebuf = (char*) malloc(16);
+	char* linebuf = malloc(16);
 	char* first_tok;
 	size_t linebuf_sz = 16;
 	size_t line_sz;
@@ -51,47 +49,48 @@ int build_and_multiply(matrix* A, matrix* B, char* filename) {
 	// free linebuf 
 	free(first_tok);
 
+	// allocate space for matrix A
+	A->data = (float*) malloc(sizeof(float) * A->height * A->width);
+	B->data = (float*) malloc(sizeof(float) * B->height * B->width);
+
 	// init line reading for matrix data
 	size_t line_len = A->width * 2 + 64;
-	char* base = (char*) malloc(line_len);
+	char* base = malloc(line_len);
 	char* line;
 
 	// read in matrix A
-	MatrixXd A_data(A->height, A->width);
 	for (int i = 0; i < A->height; i++) {
-
 		line_sz = getline(&base, &line_len, mfile);
 		line = base;
 
 		for (int j = 0; j < A->width; j++) {
 			first_tok = strsep(&line, " ");
-			A_data(i,j) = atoi(first_tok);
+			A->data[i*A->height+j] = atof(first_tok);
 		}
 	}
 
 	// gross, i know. but reset for matrix B
 	free(base);
 	line_len = B->width * 2 + 64;
-	base = (char*) malloc(line_len);
+	base = malloc(line_len);
 
 	// read in matrix B
-	MatrixXd B_data(B->height, B->width);
 	for (int i = 0; i < B->height; i++) {
-
 		line_sz = getline(&base, &line_len, mfile);
 		line = base;
 
 		for (int j = 0; j < B->width; j++) {
 			first_tok = strsep(&line, " ");
-			B_data(i,j) = atoi(first_tok);
+			B->data[i*B->height+j] = atof(first_tok);
 		}
 	}
 
 	free(base);
 
-	// MULTIPLY
-
-	MatrixXd C_data = A_data * B_data;
+	// build C
+	C->height = A->height;
+	C->width = B->width;
+	C->data = (float*) malloc(sizeof(float) * C->height * C->width);
 
 	return 0;
 }
@@ -110,10 +109,25 @@ int main(int argc, char** argv) {
 
 	matrix A;
 	matrix B;
+	matrix C;
 
-	if (build_and_multiply(&A, &B, argv[1]) < 0) {
-		printf("Could not build and multiply matrices\n");
+	if (build_matrices(&A, &B, &C, argv[1]) < 0) {
+		printf("Could not build matrices\n");
 		return 2;
+	}
+
+	// MULTIPLY
+
+	cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 
+		A->height, B->width, A->width, 1, 
+		A->data, A->height, B->data, B->height, 
+		1, C->data, C->height);
+
+	for (int i = 0; i < C->height; i++) {
+		for (int j = 0; j < C->width; j++) {
+			printf("%f ", C->data[i*C->height+j]);
+		}
+		printf("\n");
 	}
 
 	return 0;
